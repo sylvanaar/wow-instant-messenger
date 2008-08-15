9ccf2520d9da9e3a6eaa6bbdb21b2089e1d7caaf
John Langone <johnlangone@gmail.com>
2008-08-15 00:04:18 +0000

    forgot to add some files to svn

---
+++ Modules/Sounds.lua
@@ -0,0 +1,8 @@
+local WIM = WIM;
+
+
+local Sounds = WIM:CreateModule("Sounds", true);
+
+function Sounds:PostEvent_Whisper(...)
+    PlaySoundFile("Interface\\AddOns\\"..WIM.addonTocName.."\\Sounds\\wisp.wav");
+end
