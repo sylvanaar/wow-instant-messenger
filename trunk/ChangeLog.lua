0af624cf51c71c1c90fb9c916df1bcccf81a5f85
pazza <pazza@70514d9e-cf7d-479a-a7bd-7141c05b143a>
2008-11-06 19:22:57 +0000

    Updated svn:external and pkgmeta to import LibBabble-TalentTree-3.0

--- trunk/ChangeLog.lua
+++ trunk/ChangeLog.lua
@@ -19,6 +19,7 @@ addEntry("3.0.4", "10/??/2008", [[
     *Tabs scroll correctly now.
     *Location button on shortcut uses special W2W tooltip if applicable.
     *History viewer wasn't displaying realms which had punctuation characters in it.
+    *Fixed bug where alerts where referencing minimap icon even though it hasn't been loaded.
 ]]);
 addEntry("3.0.3", "10/23/2008", [[
     +Added Tab Management module. (Auto grouping of windows.)
