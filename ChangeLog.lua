97a81eb21e137d70c08389777a066b1d68817167
John Langone <johnlangone@gmail.com>
2009-03-11 05:45:49 +0000

    *code optimization in tab handler. please pay attention to tabs. make sure I didn't break anything. Thanks.

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -16,7 +16,7 @@ addEntry("3.0.7", "03/??/2008", [[
     +Added context menu management system.
     +Added minimap icon right mouse click menu. (not completed).
     *Modules were getting loaded on startup regardless of their settings.
-    *Fixed a bug where tabs would be updated incorrectly.
+    *Fixed a bug where tabs would be updated incorrectly. 
 ]]);
 
 addEntry("3.0.6", "03/10/2008", [[
