4b7bd19d3445e03798a1c573e5361c02556b5f4b
Adam <MysticalOS@users.noreply.github.com>
2013-11-09 07:37:54 +0000

    Now that I have my addon tools working again I can bump bump version and tag GetUnitName fixes from other day. :) (only took hex editing nvidia drivers to fix an instability in opencl).

--- WIM.lua
+++ WIM.lua
@@ -14,7 +14,7 @@ setfenv(1, WIM);
 
 -- Core information
 addonTocName = "WIM";
-version = "3.6.11";
+version = "3.6.12";
 beta = false; -- flags current version as beta.
 debug = false; -- turn debugging on and off.
 useProtocol2 = true; -- test switch for new W2W Protocol. (Dev use only)
@@ -465,7 +465,7 @@ function IsInParty(user)
 end
 
 function IsInRaid(user)
-    for i=1, 40 do
+    for i=1, GetNumGroupMembers() do
         if(_G.GetUnitName("raid"..i, true) == user) then
             return true;
         end
