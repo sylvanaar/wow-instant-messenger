0e9b6a99ab8f380b92ea6007454fd4316fee4ad7
Adam <MysticalOS@users.noreply.github.com>
2015-10-23 20:02:53 +0000

    Update url handler patterns

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -16,6 +16,9 @@ local function addBetaEntry(version, rdate, description, transmitted)
 end
 
 -- ChangeLog Entries.
+addEntry("3.6.28", "10/23/2015", [[
+	*Updated URL handler patterns to parse URLs better.
+]]);
 addEntry("3.6.27", "06/23/2015", [[
 	*Tabs should no longer incorrectly cascade.
 	*Updated Interface version to 6.2.
@@ -35,11 +38,6 @@ addEntry("3.6.23", "01/02/2015", [[
 ]]);
 addEntry("3.6.22", "12/28/2014", [[
 	*Fixed 3 more missing options in 'Display Settings'
-]]);
-addEntry("3.6.21", "11/30/2014", [[
-	*WIM should no longer taint join battleground.
-	*The window behavior options should be visible once again. Not as pretty as they used to be though.
-	*Armory links should go to correct region now for US players.
 ]]);
 
 
