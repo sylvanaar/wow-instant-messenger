fdb5a037ce74da5307291c17a610eae7e1099e3f
MysticalOS <mysticalosx@gmail.com>
2019-09-26 13:07:21 -0400

    Fix bnet shortcut bar invite button on retail

--- Modules/ShortcutBar.lua
+++ Modules/ShortcutBar.lua
@@ -342,7 +342,7 @@ RegisterShortcut("invite", L["Invite to Party"], {
 		OnClick = function(self)
 			local win = self.parentWindow;
 			if win.isBN then
-				_G.FriendsFrame_BattlenetInvite(nil, win.bn.id)
+				_G.FriendsFrame_InviteOrRequestToJoin and _G.FriendsFrame_InviteOrRequestToJoin(nil, win.bn.id) or _G.FriendsFrame_BattlenetInvite(nil, win.bn.id)
 			else
 				_G.InviteUnit(win.theUser)
 			end
