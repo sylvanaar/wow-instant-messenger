bc10b42d7d9b1357c600e83b70b12aad233f4862
MysticalOS <mysticalosx@gmail.com>
2021-03-31 22:46:31 -0400

    Now compatible with TBC Beta

--- Modules/ShortcutBar.lua
+++ Modules/ShortcutBar.lua
@@ -354,7 +354,7 @@ RegisterShortcut("invite", L["Invite to Party"], {
 					_G.FriendsFrame_BattlenetInvite(nil, win.bn.id)
 				end
 			else
-				if isShadowlands then
+				if isShadowlands and not isTBC then
 					_G.C_PartyInfo.InviteUnit(win.theUser)
 				else
 					_G.InviteUnit(win.theUser)
