6a7957958e3f8bc89e33e646a8092eed2313e6bd
MysticalOS <mysticalosx@gmail.com>
2021-03-31 23:27:37 -0400

    Remove redundant check

--- Modules/ShortcutBar.lua
+++ Modules/ShortcutBar.lua
@@ -354,7 +354,7 @@ RegisterShortcut("invite", L["Invite to Party"], {
 					_G.FriendsFrame_BattlenetInvite(nil, win.bn.id)
 				end
 			else
-				if isShadowlands and not isTBC then
+				if isShadowlands then
 					_G.C_PartyInfo.InviteUnit(win.theUser)
 				else
 					_G.InviteUnit(win.theUser)
